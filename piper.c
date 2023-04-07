e5fe92277d612ad04c97aa42bb7d88b72da56297
flopezmenardi <flopezmenardi@itba.edu.ar>
2023-04-07 14:08:42 -0300

    creacion de slaves/pipes

--- piper.c
+++ piper.c
@@ -14,63 +14,48 @@ int main(int argc, char const * argv[]){
     char * tests[] = {"tests.h", NULL };
     char * envf[] = { NULL };
     char * envs[] = { NULL };
-    int pipem2s[2];
-    int pipes2m[2];
-    pipe(pipem2s);
-    pipe(pipes2m);
-
     
 
     if(fork() == 0) {       // conecta el stdout de este
         //MASTER
-        //cierro STDOUT, reemplazo por w-end del pipe master to slave (el que pasa paths de archivos)
-        close(0);
-        dup(pipem2s[0]);
-        close(pipem2s[0]); //cierro el duplicado
+        int n = 5;
+        int slave2master[n][2]; //fds de pipe que slave retorna rta   -> 0 read, 1 write
+        int master2slave[n][2]; //fds de pipe que master manda archivos a slave
+        for(int i = 0; i < n; i++){
+            pipe(slave2master[i]);
+            pipe(master2slave[i]);
+        }
 
-        //cierro STDIN, reemplazo por r-end del pipe slave to master (slave pasa hash, master recibe)
-        close(1);
-        dup(pipes2m[1]);
-        close(pipes2m[1]); //cierro el duplicado
+        //creacion (fork) de los n procesos slave
+        for(int i = 0; i < n; i++){
+            if(fork() == 0){
+                //SLAVE numero i
+                close(slave2master[i][0]); //en pipe de slave2master, cierro el read
+                close(master2slave[i][1]); //en pipe de master2slave, cierro el write
+                dup2(master2slave[i][0], 0); //reemplazo stdin por el read del pipe master2slave
+                dup2(slave2master[i][1], 1); //reemplazo stdout por el write del pipe slave2master
+                
+                for(int j = 0; j < n; j++){
+                    if(j != i){
+                        close(slave2master[j][0]);
+                        close(slave2master[j][1]);
+                        close(master2slave[j][0]);
+                        close(master2slave[j][1]);
+                    }
+                    
+                }
+                execve("testf.h", testf, envf);
+            } else{
+                //Error creando slave
+            }
+        }
 
-        //cierro los que sobran a los que no deberia tener acceso el master:
-        close(pipem2s[1]);
-        close(pipes2m[0]);
-        
-        
-        // close(pipefd[0]);
-        // close(1);
-        // dup(pipefd[1]);
-        // close(pipefd[1]);
-        execve("tests.h", tests, envs);
-    } else if (fork() == 0) {
-        //SLAVE
-        //cierro STDOUT, reemplazo por w-end del pipe slave to master
-        close(0);
-        dup(pipes2m[0]);
-        close(pipes2m[0]); //cierro duplicado
-
-        //cierro STDIN, reemplazo por el r-end del pipe master to slave
-        close(1);
-        dup(pipem2s[1]);
-        close(pipem2s[1]); //cierro duplicado
-
-        //cierro los que sobran a los que no deberia tener acceso el master:
-        close(pipem2s[0]);
-        close(pipes2m[1]);
-
-        // close(pipefd[1]);           // al stdin de este
-        // close(0);
-        // dup(pipefd[0]);
-        // close(pipefd[1]);
-        execve("testf.h", testf, envf);
-    } else {
-        close(3);
-        close(4);
+    } else{
+        //Error creando master
     }
 
     while((wpid = waitpid(-1, &status, 0)) > 0);
-
+    while(1);
     printf("Bofadeez\n");
 
     return 0;
