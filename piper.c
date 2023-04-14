6162d3ab484a40ed84c03a77972f8a13fe48075c
flopezmenardi <flopezmenardi@itba.edu.ar>
2023-04-14 14:56:30 -0300

    initial files fixed | added comments

--- piper.c
+++ piper.c
@@ -1,3 +1,5 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "piper.h"
 
 #define PIPE_R_END 0
@@ -18,29 +20,30 @@ int main(int argc, char const * argv[]){
     char * tests[] = {"tests", NULL };
     char * envs[] = { NULL };
 
-    if (argc < 2) {                     // si no se pasan archivos a hashear
+    if (argc < 2) {                     // if no files are sent to be hashed
         printf("Usage: %s file1 file2 ... fileN\n", argv[0]);
         return 1;
     }
 
-    const int n = calculateSlaves(argc-1);
-    const int initialFiles = calculateInitialFiles(argc-1, n);
+    const int slaveCount = calculateSlaves(argc-1);
+    const int initialFiles = calculateInitialFiles(argc-1, slaveCount);
     
-    allocateMem(n);                     // reserva la memoria en base a n, la cantidad de slaves
+    allocateMem(slaveCount);                     // allocates memory based on the amount of slaves
 
-    setPipes(n);                        // crea los 2 * n pipes que se van a comunicar master - slave[]
+    setPipes(slaveCount);                        // creates the 2 * (slaveCount) pipes that will communicate master - slave
 
-    //creacion (fork) de los n procesos slave
-    for(int i = 0; i < n; i++){
+    //creation of slaveCount amount of slaves (fork)
+    for(int i = 0; i < slaveCount; i++){
         slavePids[i] = fork();
         if(slavePids[i] == 0){
-            //SLAVE numero i
-            dup2(master2slave[i][PIPE_R_END], 0); //reemplazo stdin por el read del pipe master2slave
-            dup2(slave2master[i][PIPE_W_END], 1); //reemplazo stdout por el write del pipe slave2master
-                
-            //cierro los fd's de los pipes para el slave
-            closePipes(n);
+            //SLAVE number i
+            dup2(master2slave[i][PIPE_R_END], 0); //replace stdin with read from master2slave pipe
+            dup2(slave2master[i][PIPE_W_END], 1); //replace stdout with write from slave2master pipe
+            
+            //closing of fd's of pipes for the slave processes
+            closePipes(slaveCount);
 
+            //run the slaves
             execve("tests", tests, envs);
         } else if (slavePids[i] == -1){
             perror("Error creando slave");
@@ -49,58 +52,53 @@ int main(int argc, char const * argv[]){
     }
 
     // cerrar el ???
-    for (int i = 0; i < n ; i++) {
+    for (int i = 0; i < slaveCount ; i++) {
         if (close(master2slave[i][PIPE_R_END]) == -1 || close(slave2master[i][PIPE_W_END]) == -1) {
             perror("Error cerrando pipes");
         }
     }
 
-    int argNumber = 1; //en argumento #1 aparece el primer path a los archivos que quiero
-    char * slaveCurrentFile[FILENAME_SIZE];         // me interesa saber que archivo tiene cada slave
+    int argNumber = 1; //arguemnt #1 is the first file to be hashed
+    char * slaveCurrentFile[FILENAME_SIZE];  //keep track of what file each slave is processing
 
-    //sem_t *initialSemaphore = sem_open("/initialSemaphore", O_CREAT, S_IRWXU, 0);
-
-
-    //envio inicial de archivos a slaves (mando solo 1 archivo, despues cambiar. Afecta a slaveCurrentFile tambien)
-    for(int i = 0; i < n; i++){
-        // if(i >= n)
-        //     sem_wait(initialSemaphore);
-        writeToSlave(i, (char *)argv[argNumber]);
-        slaveCurrentFile[i] = (char *)argv[argNumber];
+    //send initial files to slaves
+    for (int i = 0; i < initialFiles; i++) { 
+        writeToSlave(i%slaveCount, (char *)argv[argNumber]);
+        slaveCurrentFile[i%slaveCount] = (char *)argv[argNumber];
         argNumber++;
     }
     
+    //keeps track of how many files have been printed
     int printedArgNumber = 0;
 
     while(printedArgNumber != argNumber - 1){  
         fd_set masterReadSet;
-        FD_ZERO(&masterReadSet); //vacío el set
-        for (int i = 0; i < n; i++) {
-            FD_SET(masterRead[i], &masterReadSet); //agrego los fds de los pipes que leo al set
+        FD_ZERO(&masterReadSet); //empty the fd set
+        for (int i = 0; i < slaveCount; i++) {
+            FD_SET(masterRead[i], &masterReadSet); //add fd's of pipes to the set
         }
-        //espero a que algun slave termine su tarea
-        int selectRet = select(masterRead[n-1] + 1, &masterReadSet, NULL, NULL, NULL);
+        //wait until a slave finishes their task
+        int selectRet = select(masterRead[slaveCount-1] + 1, &masterReadSet, NULL, NULL, NULL);
         if (selectRet == -1) {
             perror("Error in select");
             exit(1);
         }
 
-        //encontrar el slave que terminó su tarea
-        for(int j=0; j < n; j++){
+        //find slave that finished their task, print their output
+        for(int j=0; j < slaveCount; j++){
             if(slavePids[j] != -1 && FD_ISSET(masterRead[j], &masterReadSet)){
                 char hashValue[256];
-                readFinalizedTask(hashValue, masterRead[j]);   //leo el resultado de la tarea que esta terminada
-                printf(hashValue);
-                //printSlave(hashValue, slavePids[j], slaveCurrentFile[j]);   // imprimo el archivo que se termino de procesar
+                readFinalizedTask(hashValue, masterRead[j]);   //read output of slave that finished task
+                printf(hashValue);                            //print output of slave that finished task
                 printedArgNumber++;
 
-                if (argNumber < argc) {                         // verifica si quedan archivos por mandar
-                    writeToSlave(j, (char *)argv[argNumber]);   //mando siguiente archivo al slave j
+                if (argNumber < argc) {                         //check if theres more files to send
+                    writeToSlave(j, (char *)argv[argNumber]);   //sending next file to slave number j
                     slaveCurrentFile[j]= (char *) argv[argNumber++];
                 }
             }
 
-            // nunca paso esto, TODO formalizar
+            // nunca paso esto, TO DO formalizar
             if(argNumber > argc){
                 printf("me llego mas de uno\n");
                 exit(1);
@@ -108,9 +106,9 @@ int main(int argc, char const * argv[]){
         }
     }
 
-    closeMaster2SlaveWrite(n);      // cierra el pipe de escritura del master, slave recibe un EOF
+    closeMaster2SlaveWrite(slaveCount);      // close writing pipe of master, slave receives EOF
 
-    freeMem(n);                     // libera la memoria reservada
+    freeMem(slaveCount);                     // free reserved memory
 
     while((wpid = waitpid(-1, &status, 0)) > 0);
 
@@ -119,7 +117,7 @@ int main(int argc, char const * argv[]){
     return 0;
 }
 
-/* devuelve la cantidad de slaves */
+/* returns amount of slaves to be used */
 int calculateSlaves(int fileCount) {
     if (fileCount < 5) {
         return fileCount;
@@ -127,6 +125,7 @@ int calculateSlaves(int fileCount) {
     return 5;
 }
 
+//returns the total number of files to be initially distributed to the slaves
 int calculateInitialFiles(int fileCount, int slaveCount) {
     int n = ceil(0.1 * (double) fileCount);
     if (n < slaveCount) {
@@ -135,7 +134,7 @@ int calculateInitialFiles(int fileCount, int slaveCount) {
     return n;
 }
 
-/* reserva memoria en base a la cantidad de slaves */
+/* reserves memory based on the amount of slaves */
 void allocateMem(int slaveCount) {
     // allocate memory for arrays
     slave2master = (int **) malloc(slaveCount * sizeof(int *));
@@ -151,7 +150,7 @@ void allocateMem(int slaveCount) {
     return;
 }
 
-/* libera la memoria dinamica utilizada */
+/* frees dinamic memory used */
 void freeMem(int slaveCount) {
     // free memory when done
     for (int i = 0; i < slaveCount; i++) {
@@ -166,7 +165,7 @@ void freeMem(int slaveCount) {
     return;
 }
 
-/* crea los pares de pipes que comunican master - slave[] */
+/* creates the 2 * (slaveCount) pipes that will communicate master - slave */
 void setPipes(int n) {
     for(int i = 0; i < n; i++){     
         if(pipe(slave2master[i]) == -1 || pipe(master2slave[i]) == -1){
@@ -177,7 +176,7 @@ void setPipes(int n) {
     }
 }
 
-/* cierra todos los pipes (lo deben llamar los hijos) */
+/* closes all the pipes. Child processes must call this function */
 void closePipes(int n) {
     for(int j = 0; j < n; j++){
             if (close(slave2master[j][PIPE_R_END]) == -1|| close(slave2master[j][PIPE_W_END]) == -1 ||
@@ -188,37 +187,35 @@ void closePipes(int n) {
     }
 }
 
-/* realiza la lectura del fd solicitado, deja el resultado en hashValue */
+/* reads from the specified fd, leaves value in hashValue*/
 void readFinalizedTask(char * hashValue, int fd) {
     if(read(fd, hashValue, 256) == -1){
         perror("Error read failed");
         exit(1);
     }
-    // if(read(fd, hashValue, 256) == -1){
-    //     perror("Error read failed");
-    //     exit(1);
-    // }
     return;
 }
 
-/* manda al esclavo correspondiente el path del archivo */
+/* sends the corresponding slave the path to the file it must hash */
 void writeToSlave(int slaveNum, char * filePath) {
-    if (write(master2slave[slaveNum][PIPE_W_END], filePath, strlen(filePath)) == -1) {
+    char toRet [256];
+    sprintf(toRet, "%s%s", filePath, "\n");
+    if (write(master2slave[slaveNum][PIPE_W_END], toRet, strlen(toRet)) == -1) {
         perror("Error: write failed");
         exit(1);
     }
 }
 
-/* imprime el valor devuelto por el slave */
+/* prints slave output */
 void printSlave( char * hashValue, pid_t slavePid, char * fileName) {
     printf("Hash value: %s del slave (PID): %i que recibio el archivo: %s\n", hashValue, slavePid, fileName);
     return;
 }
 
-/* cierra los pipes de escritura del master, mandando un EOF */
+/* closes master's writing pipes, sending EOF */
 void closeMaster2SlaveWrite(int n) {
     for (int i = 0; i < n; i++) {
-        if (close(master2slave[i][PIPE_W_END]) == -1) { //cerrar los pipes de escritura del master    
+        if (close(master2slave[i][PIPE_W_END]) == -1) { //closing of master's writing pipes    
             perror("Close");
             exit(1);
         }
--- tests.c
+++ tests.c
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,45 +13,65 @@
 #define BUF_SIZE 1024
 #define HASH_MD5_SIZE 32
 
+//receives filepath, calculates hash, adds its PID and file name, writes to stdout (slave2master pipe)
+void calculateHash(char *token) {
+    char hexHash[HASH_MD5_SIZE + 1];
+    char cmd[BUF_SIZE+12];
+    sprintf(cmd, "md5sum %s", token); 
+
+    FILE *fp = popen(cmd, "r");
+    if (fp == NULL) {
+        printf("Error: Could not execute command %s\n", cmd);
+        return;
+    }
+    if (fscanf(fp, "%32s", hexHash) != 1) {
+        printf("Error: Could not read hash from output of command %s\n", cmd);
+        pclose(fp);
+        return;
+    }
+    pclose(fp);
+
+    char pidString[BUF_SIZE]; 
+    sprintf(pidString, "%d", getpid()); //stores pid in string pidString
+
+    char toWrite[BUF_SIZE * 4];
+    //concatenates all relevant information to write to stdout
+    sprintf(toWrite, "Hash value: %s from slave (PID): %s of the file: %s\n",
+            hexHash, pidString, token);
+
+    //writes to stdout
+    if (write(STDOUT_FILENO, toWrite, strlen(toWrite)+1) < 0) {
+        printf("Error: Could not write hash to stdout\n");
+        return;
+    }
+}
+
+//receives input read in main function, will parse it and send filenames 
+void processInput(char *input) {
+    char token[256] = "";
+    int tokenPos = 0;
+    for (int i = 0; i < strlen(input); i++) {
+        if (input[i] == '\n') {
+            token[tokenPos] = '\0'; // null terminated
+            calculateHash(token);
+            // reset token
+            memset(token, 0, sizeof(token));
+            tokenPos = 0;
+        } else {
+            //copy each char to token
+            token[tokenPos++] = input[i];
+        }
+    }
+}
+
 int main() {
     char filePath[BUF_SIZE];
-    char hexHash[HASH_MD5_SIZE + 1];
     int bytesRead;
 
-    //sem_t *initialSemaphore = sem_open("/initialSemaphore", O_CREAT, S_IRWXU, 0);
-
+    //reads from pipe (master2slave), there may be more than one file read, they are separated by '\n'
     while ((bytesRead = read(STDIN_FILENO, filePath, BUF_SIZE)) > 0) {
-        //sem_post(initialSemaphore);
         filePath[bytesRead] = '\0'; // null terminated
-
-        // calculate hash
-        char cmd[BUF_SIZE+12];
-        sprintf(cmd, "md5sum %s", filePath);
-
-        FILE *fp = popen(cmd, "r");
-        if (fp == NULL) {
-            printf("Error: Could not execute command %s\n", cmd);
-            continue;
-        }
-        if (fscanf(fp, "%32s", hexHash) != 1) {
-            printf("Error: Could not read hash from output of command %s\n", cmd);
-            pclose(fp);
-            continue;
-        }
-        pclose(fp);
-
-        char pidString[BUF_SIZE];
-        sprintf(pidString, "%d", getpid());
-
-        char toWrite[BUF_SIZE * 4];
-        sprintf(toWrite, "Hash value: %s del slave (PID): %s que recibio el archivo: %s\n",
-                hexHash, pidString, filePath);
-        
-        
-        if (write(STDOUT_FILENO, toWrite, strlen(toWrite)+1) < 0) {
-            printf("Error: Could not write hash to stdout\n");
-            break;
-        }
+        processInput(filePath);
     }
 
     if (bytesRead == 0) {
